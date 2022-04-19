f6b66bd2205d924c901a52395f264dec1b62e506
Karim Shebl <karimshebl003@gmail.com>
2022-04-18 19:44:19 -0700

    added merge, darken & lighten, shrink and blur functions

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 unsigned char image[SIZE][SIZE];
+unsigned char image2[SIZE][SIZE];//necessary for merge
 
 void loadImage () {
    char imageFileName[100];
@@ -17,6 +18,20 @@ void loadImage () {
    // Add to it .bmp extension and load image
    strcat (imageFileName, ".bmp");
    readGSBMP(imageFileName, image);
+}
+
+//necessary for merge
+void loadSecondImage()
+{
+   char imageFileName[100];
+
+   // Get gray scale image file name
+   cout << "Enter the image file name: ";
+   cin >> imageFileName;
+
+   // Add to it .bmp extension and load image
+   strcat (imageFileName, ".bmp");
+   readGSBMP(imageFileName, image2);
 }
 
 void saveImage () {
@@ -133,6 +148,105 @@ void detectImageEdges(){
     }
 }
 
+void mergeTwoPictures()
+{
+    // Load the second image to merge
+    loadSecondImage();
+    for(int i = 0; i < SIZE; i++)
+    {
+        for(int j = 0; j < SIZE; j++)
+        {
+            image[i][j] = (image[i][j] + image2[i][j])/2;
+        }
+    }
+}
+
+void darken_Lighten()
+{
+    string s;
+    cout << "To darken an image press d\nTo lighten an image press l\n";
+    cin >> s;
+    if (s ==  "d")
+    {
+        for(int i = 0; i < SIZE; i++)
+        {
+            for(int j = 0; j < SIZE; j++)
+            {
+                image[i][j] *= 0.5;
+            }
+        }
+    }
+    else if (s == "l")
+    {
+        for(int i = 0; i < SIZE; i++)
+        {
+            for(int j = 0; j < SIZE; j++)
+            {
+                if (image[i][j] < 170)
+                {
+                    image[i][j] *= 1.5;
+                }
+                else
+                {
+                    image[i][j] = 255;
+                }
+            }
+        }
+    }
+}
+
+void shrinkImage()
+{
+    for(int i = 0; i < SIZE; i++)
+    {
+        for(int j = 0; j < SIZE; j++)
+        {
+            image2[i][j] = image[i][j];
+        }
+    }
+    int index_i, index_j, value, new_size;
+    cout << "enter a shrink value : ";
+    cin >> value;
+    new_size = SIZE / value;
+    for(int i = 0; i < SIZE; i++)
+    {
+        for(int j = 0; j < SIZE; j++)
+        {
+            if (i < new_size && j < new_size)
+            {
+                image[i][j] = image2[index_i][index_j];
+                index_j += value;
+            }
+            else
+            {
+                image[i][j] = 255;
+            }
+        }
+        index_j = 1;
+        index_i += value;
+    }
+}
+
+void blurImage()
+{
+    //Gaussian blur algorithm
+    for(int i = 0; i < SIZE; i++)
+    {
+        for(int j = 0; j < SIZE; j++)
+        {
+            image2[i+1][j+1] = image[i][j];
+        }
+    }
+
+    for(int i = 0; i < SIZE; i++)
+    {
+        for(int j = 0; j < SIZE; j++)
+        {
+            image[i][j] = (image2[i][j]+image2[i][j+1]+image2[i][j+2]+image2[i+1][j]+image2[i+1][j+2]+image2[i+2][j]+image2[i+2][j+1]+image2[i+2][j+2])/9;
+        }
+    }
+}
+
 int main()
 {
 
@@ -145,77 +259,90 @@ while (isVAlid || contin){
     cout<<"\n\n";
     cout<<"Enter The Number of the operation \n";
     cout<<"1- Black & White Filter \n";
+    cout<<"3- Merge Images \n";
     cout<<"4- Flip Image \n";
+    cout<<"6- Darken & Lighten an image \n";
     cout<<"7- Detect Image Edges \n";
+    cout<<"9- Shrink an Image \n";
     cout<<"a- Mirror 1/2 Image \n";
+    cout<<"c- Blur an Image \n";
     cout<<"s- Save the image to a file \n";
     cout<<"0- Exit \n\n";
     cout<<">>>> ";
     cin>>choice;
 
-if (choice == "1"){
-   ConvertBlackAndWhite();
-   isVAlid = false;
-}
+    if (choice == "1"){
+       ConvertBlackAndWhite();
+       isVAlid = false;
+    }
+    else if(choice =="3"){
+        mergeTwoPictures();
+    }
+    else if(choice =="4"){
+        cout<<"Flip (h)orizontally or (v)ertically ? \n";
+        cin>>flipDetect;
 
-else if(choice =="4"){
-    cout<<"Flip (h)orizontally or (v)ertically ? \n";
-    cin>>flipDetect;
+        if(flipDetect == "h" || flipDetect == "horizontally"){
+            flipImageHorizontally();
+            isVAlid = false;
+        }
 
-    if(flipDetect == "h" || flipDetect == "horizontally"){
-        flipImageHorizontally();
+        else if(flipDetect == "v" || flipDetect == "vertically"){
+            flipImageVertically();
+            isVAlid = false;
+        }
+    }
+    else if(choice =="6"){
+        darken_Lighten();
+    }
+    else if(choice =="7"){
+        detectImageEdges();
         isVAlid = false;
     }
-
-    else if(flipDetect == "v" || flipDetect == "vertically"){
-        flipImageVertically();
-        isVAlid = false;
+    else if(choice =="9"){
+        shrinkImage();
     }
-}
+    else if (choice == "a"){
+        cout<<"Mirror (l)eft, (r)ight, (u)pper, (d)own side? \n";
+        cin>>mirordetect;
 
-else if(choice =="7"){
-    detectImageEdges();
-    isVAlid = false;
-}
+        if(mirordetect=="d" || mirordetect == "down"){
+            mirrorDown();
+            isVAlid = false;
+        }
 
-else if (choice == "a"){
-    cout<<"Mirror (l)eft, (r)ight, (u)pper, (d)own side? \n";
-    cin>>mirordetect;
+        else if(mirordetect == "u" || mirordetect == "up"){
+            mirrorUp();
+            isVAlid = false;
+        }
 
-    if(mirordetect=="d" || mirordetect == "down"){
-        mirrorDown();
-        isVAlid = false;
+        else if(mirordetect == "l" || mirordetect == "left"){
+            mirrorLeft();
+            isVAlid = false;
+        }
+
+        else if(mirordetect == "r" || mirordetect == "right"){
+            mirrorRight();
+            isVAlid = false;
+        }
+    }
+    else if(choice == "c"){
+        blurImage();
     }
 
-    else if(mirordetect == "u" || mirordetect == "up"){
-        mirrorUp();
+    else if (choice == "s"){
+        saveImage();
         isVAlid = false;
+        contin = false;
     }
-
-    else if(mirordetect == "l" || mirordetect == "left"){
-        mirrorLeft();
+    else if (choice == "0") {
+        cout<<"HAVE A NICE DAY !! \n";
         isVAlid = false;
+        contin = false;
     }
-
-    else if(mirordetect == "r" || mirordetect == "right"){
-        mirrorRight();
-        isVAlid = false;
+    else {
+        cout<<"Please enter a VALID number ";
     }
-}
-
-else if (choice == "s"){
-    saveImage();
-    isVAlid = false;
-    contin = false;
-}
-else if (choice == "0") {
-    cout<<"HAVE A NICE DAY !! \n";
-    isVAlid = false;
-    contin = false;
-}
-else {
-    cout<<"Please enter a VALID number ";
-}
 }
 
     return 0;
